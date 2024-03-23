43c51d3c09a9f48bef7c0e57359bb8803bc32118
Rioting Pacifist <linux@riotingpacifist.net>
2024-03-23 03:08:17 -0700

    add PDF and info about LL measure

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,5 +2,6 @@
 SITENAME=$1
 echo "Sycning files"
 aws s3 cp --content-type text/html html/index.html "s3://${SITENAME}/"
+aws s3 cp --content-type application/pdf html/Right2Organize.pdf "s3://${SITENAME}/"
 for dir in assets bootstrap ; do aws s3 cp --recursive --content-type text/javascript "html/${dir}/" "s3://${SITENAME}/${dir}" ; done
 for dir in css bootstrap_theme ; do aws s3 cp --recursive --content-type text/css "html/${dir}/" "s3://${SITENAME}/${dir}" ; done
